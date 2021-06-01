2f96eec2d295fb078882a7cba60e2cdbc34f4d4e
Alharb7 <76629405+hassanalharbi123@users.noreply.github.com>
2021-06-01 13:27:39 +0300

    Add files via upload

---
+++ 1,0.py
@@ -0,0 +1,41 @@
+from os import replace
+import pyfiglet
+pyfiglet.print_figlet("TEXT -> 1,0")
+
+print("""
+                 _____________________
+                |         AL7         |
+                |                     |
+                |   alhassanAlharb7   |
+                |                     |
+                |_____________________|
+                """)
+input=input("Text >> ")
+a=input.replace("a","01100001 ")
+b=a.replace("b","01100010 ")
+c=b.replace("c","01100011 ")
+d=c.replace("d",'01100100 ')
+e=d.replace("e","01100101 ")
+f=e.replace("f",'01100110 ')
+g=f.replace("g",'01100111 ')
+h=g.replace("h",'01101000 ')
+i=h.replace("i",'01101001 ')
+j=i.replace('j','01101010 ')
+k=j.replace("k",'01101011 ')
+l=k.replace("l",'01101100 ')
+m=l.replace("m",'01101101 ')
+n=m.replace("n",'01101110 ')
+o=n.replace("o",'01101111 ')
+p=o.replace('p','01110000 ')
+q=p.replace('q','01110001 ')
+r=q.replace('r','01110010 ')
+s=r.replace("s",'01110011 ')
+t=s.replace('t','01110100 ')
+u=t.replace('u','01110101 ')
+v=u.replace('v','01110110 ')
+w=v.replace('w','01110111 ')
+x=w.replace('x','01111000 ')
+y=x.replace('y','01111001 ')
+z=y.replace('z','01111010 ')
+
+print(z)
